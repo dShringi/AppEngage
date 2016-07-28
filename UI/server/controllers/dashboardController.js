8b88a82cf505f4b03050104d3387bcdd7242b362
Smitaj S. Pawar <Smitaj102590@Mastek.com>
2016-07-28 17:18:09 +0530

    Added Comments- added 2 api. getSessionInsight and getUserInsight

--- UI/server/controllers/dashboardController.js
+++ UI/server/controllers/dashboardController.js
@@ -3,6 +3,177 @@ var config      = require('../../config/config');
 var common 		= require('../../commons/common.js');
 var async       = require('async');
 
+module.exports.getUserInsights = function(req,res){
+
+var sd = req.query["sd"],ed = req.query["ed"],akey =req.query["akey"];
+var t="D",diffDays;
+var tempstring=[],tempstr="";
+var startdate = new Date(0),enddate = new Date(0);
+var tse=0,te=0,tuu=0,tnu=0,tts=0,tce=0,dType,dailyCount=0,weeklyCount=0,monthlyCount=0,yearlyCount=0,hourlyCount=0;
+ startdate.setUTCSeconds(sd);
+ enddate.setUTCSeconds(ed);
+
+var dd = '0'.concat(startdate.getDate()).slice(-2),mm = '0'.concat(startdate.getMonth()+1).slice(-2),yyyy = startdate.getFullYear(),hh='0'.concat(startdate.getHours()).slice(-2);
+sd = ''+yyyy+mm+dd+hh;
+var sdtemp=''+yyyy+mm+dd;
+var testsd = '0'.concat(startdate.getDate()).slice(-2);
+var tempdatesd=yyyy+"-"+mm+"-"+testsd;
+var dd = '0'.concat(enddate.getDate()).slice(-2),mm = '0'.concat(enddate.getMonth()+1).slice(-2),yyyy = enddate.getFullYear(),hh='0'.concat(enddate.getHours()).slice(-2);
+ed = ''+yyyy+mm+dd+hh;
+var edtemp=''+yyyy+mm+dd;
+var tested = '0'.concat(enddate.getDate()).slice(-2);
+var tempdateed=yyyy+"-"+mm+"-"+tested;
+
+var db = mongojs(config.connectionstring+akey);
+var typeListarray=[];
+async.waterfall(
+    [	
+		
+	function(callback){ //callback start
+	
+	diffDays=common.getDateDiffernce(tempdatesd,tempdateed);  //to find no of days between two dates
+	callback(null);
+	}, //callback end
+	
+	function(callback){ //callback start
+	if(diffDays==0){ //for weekly fetch
+	t="H";
+	callback(null);
+	} 
+	
+	else { 
+	t="D";
+	sd = sdtemp;
+	ed = edtemp;
+	callback(null);
+	}
+	}, //callback end
+	
+	
+	function(callback) { //callback start
+			
+			//console.log("t value :" + t);
+			db.collection(config.coll_dashboard).aggregate([
+			{ $match: { $and: [{'_id.ty': t},{ '_id.key': { $gte: parseInt(sd), $lte: parseInt(ed) }}]  } },
+			{ $group: {_id :  "$_id.key", 'tuu' : {$sum : "$val.tuu"},'tts' : {$sum : "$val.tts"}}},
+			{ $sort: {'_id': 1 } },
+			{ $project: {_id:1,tuu:1,tts:1}}
+			],function(err, result) {
+			
+				//console.log(result);
+				if(result!=null){
+				for(var i=0;i<result.length;i++){
+					tempstring[i] = (' {'+' "date": "'+result[i]._id+'","tuu": "' +result[i].tuu + '","tts": "' +result[i].tts);
+					
+					tempstr+=tempstring[i].concat(',');
+				}
+				 tempstr = tempstr.substr(0,tempstr.length-1);
+				 //console.log(tempstr);
+				finaldetailstr='[ '+ tempstr + ']'
+				console.log(finaldetailstr);
+				}else{
+				db.close();
+				return res.json('[]');
+				}
+				callback(null);
+		});
+		
+	},//callback end
+	function(callback) { //callback start
+	db.close();
+	return res.json(finaldetailstr);
+	
+	}
+]);
+
+}
+
+module.exports.getSessionInsights = function(req,res){
+
+var sd = req.query["sd"],ed = req.query["ed"],akey =req.query["akey"];
+var t="D",diffDays;
+var tempstring=[],tempstr="";
+var startdate = new Date(0),enddate = new Date(0);
+var tse=0,te=0,tuu=0,tnu=0,tts=0,tce=0,dType,dailyCount=0,weeklyCount=0,monthlyCount=0,yearlyCount=0,hourlyCount=0;
+ startdate.setUTCSeconds(sd);
+ enddate.setUTCSeconds(ed);
+
+var dd = '0'.concat(startdate.getDate()).slice(-2),mm = '0'.concat(startdate.getMonth()+1).slice(-2),yyyy = startdate.getFullYear(),hh='0'.concat(startdate.getHours()).slice(-2);
+sd = ''+yyyy+mm+dd+hh;
+var sdtemp=''+yyyy+mm+dd;
+var testsd = '0'.concat(startdate.getDate()).slice(-2);
+var tempdatesd=yyyy+"-"+mm+"-"+testsd;
+var dd = '0'.concat(enddate.getDate()).slice(-2),mm = '0'.concat(enddate.getMonth()+1).slice(-2),yyyy = enddate.getFullYear(),hh='0'.concat(enddate.getHours()).slice(-2);
+ed = ''+yyyy+mm+dd+hh;
+var edtemp=''+yyyy+mm+dd;
+var tested = '0'.concat(enddate.getDate()).slice(-2);
+var tempdateed=yyyy+"-"+mm+"-"+tested;
+
+var db = mongojs(config.connectionstring+akey);
+var typeListarray=[];
+async.waterfall(
+    [	
+		
+	function(callback){ //callback start
+	
+	diffDays=common.getDateDiffernce(tempdatesd,tempdateed);  //to find no of days between two dates
+	callback(null);
+	}, //callback end
+	
+	function(callback){ //callback start
+	if(diffDays==0){ //for weekly fetch
+	t="H";
+	callback(null);
+	} 
+	
+	else { 
+	t="D";
+	sd = sdtemp;
+	ed = edtemp;
+	callback(null);
+	}
+	}, //callback end
+	
+	
+	function(callback) { //callback start
+			
+			//console.log("t value :" + t);
+			db.collection(config.coll_dashboard).aggregate([
+			{ $match: { $and: [{'_id.ty': t},{ '_id.key': { $gte: parseInt(sd), $lte: parseInt(ed) }}]  } },
+			{ $group: {_id :  "$_id.key", 'tse' : {$sum : "$val.tse"},'tts' : {$sum : "$val.tts"}}},
+			{ $sort: {'_id': 1 } },
+			{ $project: {_id:1,tse:1,tts:1}}
+			],function(err, result) {
+			
+				//console.log(result);
+				if(result!=null){
+				for(var i=0;i<result.length;i++){
+					tempstring[i] = (' {'+' "date": "'+result[i]._id+'","tse": "' +result[i].tse + '","tts": "' +result[i].tts);
+					
+					tempstr+=tempstring[i].concat(',');
+				}
+				 tempstr = tempstr.substr(0,tempstr.length-1);
+				 
+				finaldetailstr='[ '+ tempstr + ']'
+				console.log(finaldetailstr);
+				}else{
+				db.close();
+				return res.json('[]');
+				}
+				callback(null);
+		});
+		
+	},//callback end
+	function(callback) { //callback start
+	db.close();
+	return res.json(finaldetailstr);
+	
+	}
+]);
+
+}
+
+
 module.exports.dashboardRealTime = function(req,res){
 
 var startdate = req.query["sd"],enddate  = req.query["ed"],akey =req.query["akey"],response="";
@@ -61,6 +232,7 @@ if(startdate == enddate){
 };
 
 module.exports.dashboardCounters = function(req,res){
+
 
 var sd = req.query["sd"],ed = req.query["ed"],akey =req.query["akey"],type=req.query["type"];	
 var t="D",diffDays;
