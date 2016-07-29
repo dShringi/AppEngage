e755c2e1f407efcb6d8dd912b38d04ed3b65efb1
Arvind <arvind@ksolves.com>
2016-07-29 23:52:40 +0530

    added new api url

--- UI/client/assets/js/custom-script.js
+++ UI/client/assets/js/custom-script.js
@@ -1,8 +1,9 @@
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
 var color = d3.scale.ordinal().range(["#f47321", "#76bce6", "#75d3c5", "#379154", "#39B4BF", "#FFE666", "#E54E67", "#F75B49"]);
-
 var todayObj = new Date(), appKey = 'MastApp';
+
+//var APIBaseURL = "http://52.206.121.100/";
+var APIBaseURL = "http://13.66.57.215/appengage/";
 
 // Get donut chart SVG object
 var getDonutSVG = function(chartHolder, width, height){
