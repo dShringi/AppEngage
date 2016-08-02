f42e1829811c45e7f7f02202195f64c4a04fb289
Smitaj S. Pawar <Smitaj102590@Mastek.com>
2016-08-02 15:59:11 +0530

    Added Comments-added coll_user collection in config file

--- UI/config/config.js
+++ UI/config/config.js
@@ -1,6 +1,7 @@
-module.exports.port             = 6000;
+module.exports.port             = 3001;
 module.exports.connectionstring = 'mongodb://localhost/';
 
 module.exports.coll_crashes	= 'coll_crashes';
 module.exports.coll_dashboard	= 'coll_dashboard';
 module.exports.coll_realtime	= 'coll_realtime';
+module.exports.coll_users	= 'coll_users';
