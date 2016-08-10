766c989fb91288eac0cdc97dba768cdd16d7b337
Smitaj S. Pawar <Smitaj102590@Mastek.com>
2016-08-10 14:50:15 +0530

    Added comments-code optimzing using moment timezone

--- UI/server/controllers/userDashboardController.js
+++ UI/server/controllers/userDashboardController.js
@@ -10,6 +10,7 @@ var db = mongojs(config.connectionstring+akey);
 var resultstring=[],resultstr="";
 var searchParam,grpvalue,finaldetailstr,finalResponce;
 var searchByArray=[];
+var deviceNumber='"'+0+'"';
 
 function aggregateCalulation(grpParam,callback){ // function to fetch userCountersounters by searchparameter
 	
@@ -89,6 +90,67 @@ async.waterfall(
 		}
 		});
 	},
+	/*
+	 //added code for update
+	 	function(callback) {
+		var db = mongojs(config.connectionstring+akey);
+		var startDate1 = "1470415571",endDate1 = "1470415726";
+		var startdate = new Date(0),enddate = new Date(0);
+		startdate.setUTCSeconds(startDate1);
+		enddate.setUTCSeconds(endDate1);
+		
+		deviceNumber="212345678931";
+		var  datePart= '0'.concat(startdate.getDate()).slice(-2),monthPart = startdate.getMonth();
+		var yyyy = startdate.getFullYear();
+		var yearVal=yyyy;
+		var yearValKey='"_'+yyyy+'._id"';
+		var yearValKeyFortse='"_'+yyyy+'.$.tse"';
+		var yearValKeyFortts='"_'+yyyy+'.$.tts"';
+		
+		console.log("datePart : "+ datePart);
+		console.log("monthPart : "+ monthPart);
+		var monthdate=monthPart+datePart;
+		console.log("combination of date month and date : "+ monthdate);
+		console.log("year value:  " + yearVal);
+		console.log("yearValKey:  " + yearValKey);
+		console.log("yearValKeyFortse:  " + yearValKeyFortse);
+		console.log("yearValKeyFortss:  " + yearValKeyFortts);
+		console.log("device number : "+ deviceNumber);
+		
+		
+		//var objopt=JSON.parse('{"_id":"212345678931","_2016":[{"_id":808}]}');
+		//console.log("my output: "+objopt._id);
+		//console.log("my output: "+objopt._2016[0]._id);
+		
+		
+		
+		db.collection(config.coll_usersTest).find().toArray(function(err, result) {
+		var arr1=[];
+		
+		
+		for (var i=0;i<result.length;i++)
+		{
+		arr1[i]=result[i];
+		//console.log(arr1[i]);
+		//console.log(result);
+		}
+		});
+		
+		//db.collection(config.coll_usersTest).update({"_id":"212345678909","_2009._id":808},{"$inc":{"_2009.$.tse":2,"_2009.$.tts":100}},function(err,res){
+	
+		//db.collection(config.coll_usersTest).update({"_id":"212345678931",yearValKey:808},{"$inc":{"_2016.$.tse":2,"_2016.$.tts":100}},function(err,res){
+	
+		  db.collection(config.coll_usersTest).update({"_id":"212345678931","_2016._id":808},{"$inc":{"_2016.$.tse":2,"_2016.$.tts":100}},function(err,res){
+		  
+		console.log(res);
+		
+		});
+		
+	
+		callback(null);
+	},
+	*/
+	//end of update code
 	
 	function(callback) { //callback start
 	db.close();
