74a7af066654d77b1e4672d1804182d926c0e796
Abhijitnair@sd-taylor.co.uk <Abhiji102491@mastek.com>
2016-08-22 17:14:33 +0530

    error.png added and customscript modified

--- UI/client/assets/js/custom-script.js
+++ UI/client/assets/js/custom-script.js
@@ -129,7 +129,7 @@ $(document).ready(function () {
 
 var showError = function(err){
     //sweetAlert("Oops...", err, "error");
-	swal({   title: "OOPS",   text: err,   imageUrl: "images/thumbs-up.jpg" });
+	swal({   title: "OOPS",   text: err,   imageUrl: "assets/img/error.png" });
 }
 
 var showDeviceModelTable = function (data, tableId) {
@@ -764,6 +764,12 @@ var sortTable = function(){
 
 //***************************** DEVICE DONUT CHARTS ***********************************
 function plotDonutChart(data, ele, arc, arcOver, pie, svg, width, color){
+	
+	var tooltip = d3.select("body")
+		.append("div")  // declare the tooltip div 
+		.attr("class", "map-tooltip shadow")              // apply the 'tooltip' class
+		.style("opacity", 0);                  // set the opacity to nil
+		
 	var g = svg.selectAll(".arc")
 						.data(pie(data))
 						.enter().append("g")
@@ -779,12 +785,15 @@ function plotDonutChart(data, ele, arc, arcOver, pie, svg, width, color){
 								.duration(1000)
 								.attr("class", "shadow")
 								.attr("d", arcOver);
+								tooltip.transition().duration(200).style("opacity", 1);	
+								tooltip.html(d.data[ele]+" : "+d.data.users).style("left", (d3.event.pageX - 23) + "px").style("top", (d3.event.pageY - 46) + "px");
 						})
 						.on("mouseleave", function (d) {
 							d3.select(this)
 								.transition()
 								.duration(1000)
 								.attr("d", arc);
+								tooltip.transition().duration(200).style("opacity", 0);
 						})
 						.style("fill", function (d) { return color(d.data[ele]); });
 
@@ -819,12 +828,15 @@ function plotDonutChart(data, ele, arc, arcOver, pie, svg, width, color){
 								.transition()
 								.duration(1000)
 								.attr("d", arcOver);
+								tooltip.transition().duration(200).style("opacity", 1);	
+								tooltip.html(d.data[ele]+" : "+d.data.users).style("left", (d3.event.pageX - 23) + "px").style("top", (d3.event.pageY - 46) + "px");
 						})
 						.on("mouseleave", function (d) {
 							d3.select(this)
 								.transition()
 								.duration(1000)
 								.attr("d", arc);
+								tooltip.transition().duration(200).style("opacity", 0);
 						})
 						.style("fill", function (d) { return color(d.data[ele]); });
 
