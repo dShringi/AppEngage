4d21401a4820e2315b5fc7bc0ece2ffa496a09f2
Sanjeev Ranjan <sanjeev13540@Mastek.com>
2016-08-24 19:45:27 +0530

    cron file added with yeary/weekly/monthly

--- NodeJS/Jobs/cronJobs/jobDashboardPopulation.js
+++ NodeJS/Jobs/cronJobs/jobDashboardPopulation.js
@@ -2,9 +2,10 @@
 'use strict'
 var mongodb = require('mongodb');
 var dateFormat = require('dateformat');
-var logger = require('./conf/log.js');
-var config = require('./conf/config.js');
-var common = require('./commons/common');
+var logger = require('././Scripts/conf/log.js');
+var config = require('././Scripts/conf/config.js');
+var common = require('././Scripts/commons/common');
+
 
 var MongoClient = mongodb.MongoClient;
 
@@ -17,79 +18,416 @@ if (Boolean(url) && Boolean(appKey) && Boolean(triggerTyep)){
 		if (err) {
 			var errMsg = common.getErrorMessageFrom(err);
             logger.error(errMsg);
+			
 		} else {
 			console.log('Connection established to', url);
-
-			var beginsCollection = db.collection('coll_begins');
-		
+			
 			var endDate=new Date();
 			var endDateUnixtime = Math.round(endDate.getTime() / 1000);
-
+			var endDateDay = endDate.getDate();
+			var endDateMonth = endDate.getMonth()+1;
+			var endDateYear = endDate.getFullYear();
+			
 			var startDate;
 			if(triggerTyep == 'Hourly' || triggerTyep == 'hourly') {
 				startDate = new Date(endDate-60*60*1000);
+				var beginsCollection = db.collection('coll_begins');
+				
+				var startDateUnixtime = Math.round(startDate.getTime() / 1000);
+				//this query will form a implicit AND (no need to decleare explicit AND)
+				var query = [{$match:{rtr:{$gte:startDateUnixtime,$lt:endDateUnixtime}}},{$group:{_id :{val:"$val.dt","val.did":"$val.did"}}},{$group:{_id:{result:'$_id.val'},"users":{$sum:1}}}];
+				beginsCollection.aggregate(query,function(err, items) {
+					if(err){
+						var errMsg = common.getErrorMessageFrom(err);
+						logger.error(errMsg);
+					}
+					var resultLength = items.length;
+					if(resultLength > 0){
+						// find tablet/smart phone entry
+						var smartCount;
+						var tabletCount;
+						for (var i in items) {
+							var count = items[i];
+							if(count._id.result == 'S') {
+								smartCount = count.users;
+							} else if(count._id.result == 'T'){
+								tabletCount = count.users;
+							}
+						}
+					
+						var KeyFormat = dateFormat(endDate, "yyyymmddHH");
+						var dashboardCollection = db.collection('coll_dashboard');
+					
+						//smart Hourly collection update
+						if(smartCount != null && smartCount > 0) {
+							var dashboardQuerySmart = '{"$and":[{"_id.ty":"H"}, {"_id.dt" : "S"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQuerySmartJson = JSON.parse(dashboardQuerySmart);
+							//var updateSmt = '{"$set":{"val.tuu":'+smartCount+'}}';
+							//var updateQueryJsonSmt = JSON.parse(updateSmt);
+							var updateSmt = {};
+							updateSmt["val.tuu"]=smartCount;
+							dashboardCollection.update(dashboardQuerySmart, updateSmt,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+				
+						//tablet phone Hourly collection update
+						if(tabletCount != null && tabletCount > 0) {
+							var dashboardQueryTablet = '{"$and":[{"_id.ty":"H"}, {"_id.dt" : "T"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQueryTabletJson = JSON.parse(dashboardQueryTablet);
+							//var updateTab = '{"$set":{"val.tuu":'+tabletCount+'}}';
+							//var updateQueryJsonTab = JSON.parse(updateTab);
+							var updateTab = {};
+							updateTab['val.tuu']=tabletCount;
+							dashboardCollection.update(dashboardQueryTablet, updateTab ,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+						console.log('all done...!');
+					} else {
+						console.log('no record found...!');
+					}
+					db.close();
+				});
 			} else if (triggerTyep == 'Daily' || triggerTyep == 'daily') {
 				startDate = new Date(endDate-(24*60*60*1000));
+				var startDateDay = startDate.getDate();
+				if(startDateDay < 10) {
+					startDateDay = '0'+startDateDay;
+				}
+				if(endDateDay < 10) {
+					endDateDay = '0'+endDateDay;
+				}
+				
+				var startDateMonth = startDate.getMonth()+1;
+				var startDateYear = startDate.getFullYear();
+				
+				var usersCollection = db.collection('coll_users');
+				var endDayMonth = parseInt(''+endDateMonth+''+endDateDay);
+				var startDayMonth = parseInt(''+startDateMonth+''+startDateDay);
+				var unwindvar = '$_'+endDateYear;
+				var matchKey = '_'+endDateYear+'._id';
+				
+				var query = [{"$match":{"_2016._id" : {"$gte":startDayMonth,"$lt":endDayMonth}}},{"$unwind": unwindvar},{"$group":{"_id":{"result" : "$ldt"},"users":{"$sum": 1}}}];
+				//var query = '[{"$match":{"'+matchKey+'" : {"$gte":'+startDayMonth+',"$lt":'+endDayMonth+'}}},{"$unwind": "'+unwindvar+'"},{"$group":{"_id":{"ldt" : "$ldt"},"users":{"$sum": 1}}}]';
+				
+				usersCollection.aggregate(query,function(err, items) {
+					if(err){
+						var errMsg = common.getErrorMessageFrom(err);
+						logger.error(errMsg);
+					}
+					var resultLength = items.length;
+					if(resultLength > 0){
+						// find tablet/smart phone entry
+						var smartCount;
+						var tabletCount;
+						for (var i in items) {
+							var count = items[i];
+							if(count._id.result == 'S') {
+								smartCount = count.users;
+							} else if(count._id.result == 'T'){
+								tabletCount = count.users;
+							}
+						}
+						
+						var KeyFormat = dateFormat(endDate, "yyyymmdd");
+						var dashboardCollection = db.collection('coll_dashboard');
+					
+						//smart Hourly collection update
+						if(smartCount != null && smartCount > 0) {
+							var dashboardQuerySmart = '{"$and":[{"_id.ty":"D"}, {"_id.dt" : "S"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQuerySmartJson = JSON.parse(dashboardQuerySmart);
+							//var updateSmt = '{"$set":{"val.tuu":'+smartCount+'}}';
+							//var updateQueryJsonSmt = JSON.parse(updateSmt);
+							var updateSmt = {};
+							updateSmt['val.tuu']=smartCount;
+							dashboardCollection.update(dashboardQuerySmart, updateSmt,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+				
+						//tablet phone Hourly collection update
+						if(tabletCount != null && tabletCount > 0) {
+							var dashboardQueryTablet = '{"$and":[{"_id.ty":"D"}, {"_id.dt" : "T"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQueryTabletJson = JSON.parse(dashboardQueryTablet);
+							//var updateTab = '{"$set":{"val.tuu":'+tabletCount+'}}';
+							//var updateQueryJsonTab = JSON.parse(updateTab);
+							var updateTab = {};
+							updateTab['val.tuu']=tabletCount;
+							dashboardCollection.update(dashboardQueryTablet, updateTab ,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+						console.log('all done...!');
+					} else {
+						console.log('no record found...!');
+					}
+					db.close();
+				});
+			} else if(triggerTyep == 'Weekly' || triggerTyep == 'weekly'){
+			
+				startDate = new Date();
+				startDate.setDate(startDate.getDate() - 7);
+				var startDateDay = startDate.getDate();
+				if(startDateDay < 10) {
+					startDateDay = '0'+startDateDay;
+				}
+				if(endDateDay < 10) {
+					endDateDay = '0'+endDateDay;
+				}
+				
+				
+				var startDateMonth = startDate.getMonth()+1;
+				var startDateYear = startDate.getFullYear();
+				
+				var usersCollection = db.collection('coll_users');
+				var endDayMonth = parseInt(''+endDateMonth+''+endDateDay);
+				var startDayMonth = parseInt(''+startDateMonth+''+startDateDay);
+				var unwindvar = '$_'+endDateYear;
+				var matchKey = '_'+endDateYear+'._id';
+				
+				var query = [{"$match":{"_2016._id" : {"$gte":startDayMonth,"$lt":endDayMonth}}},{"$unwind": unwindvar},{"$group":{"_id":{"result" : "$ldt"},"users":{"$sum": 1}}}];
+				//var query = '[{"$match":{"'+matchKey+'" : {"$gte":'+startDayMonth+',"$lt":'+endDayMonth+'}}},{"$unwind": "'+unwindvar+'"},{"$group":{"_id":{"ldt" : "$ldt"},"users":{"$sum": 1}}}]';
+				
+				usersCollection.aggregate(query,function(err, items) {
+					if(err){
+						var errMsg = common.getErrorMessageFrom(err);
+						logger.error(errMsg);
+					}
+					var resultLength = items.length;
+					if(resultLength > 0){
+						// find tablet/smart phone entry
+						var smartCount;
+						var tabletCount;
+						for (var i in items) {
+							var count = items[i];
+							if(count._id.result == 'S') {
+								smartCount = count.users;
+							} else if(count._id.result == 'T'){
+								tabletCount = count.users;
+							}
+						}
+						
+						var KeyFormat = dateFormat(endDate, "yyyymmddHH");
+						var dashboardCollection = db.collection('coll_dashboard');
+					
+						//smart Hourly collection update
+						if(smartCount != null && smartCount > 0) {
+							var dashboardQuerySmart = '{"$and":[{"_id.ty":"W"}, {"_id.dt" : "S"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQuerySmartJson = JSON.parse(dashboardQuerySmart);
+							//var updateSmt = '{"$set":{"val.tuu":'+smartCount+'}}';
+							//var updateQueryJsonSmt = JSON.parse(updateSmt);
+							var updateSmt = {};
+							updateSmt['val.tuu']=smartCount;
+							dashboardCollection.update(dashboardQuerySmart, updateSmt,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+				
+						//tablet phone Hourly collection update
+						if(tabletCount != null && tabletCount > 0) {
+							var dashboardQueryTablet = '{"$and":[{"_id.ty":"W"}, {"_id.dt" : "T"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQueryTabletJson = JSON.parse(dashboardQueryTablet);
+							//var updateTab = '{"$set":{"val.tuu":'+tabletCount+'}}';
+							//var updateQueryJsonTab = JSON.parse(updateTab);
+							var updateTab = {};
+							updateTab['val.tuu']=tabletCount;
+							dashboardCollection.update(dashboardQueryTablet, updateTab ,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+						console.log('all done...!');
+					} else {
+						console.log('no record found...!');
+					}
+					db.close();
+				});
+			
+			} else if(triggerTyep == 'Monthly' || triggerTyep == 'monthly'){
+				startDate = new Date(endDate.getFullYear(), endDate.getMonth(), 1);
+				var startDateDay = startDate.getDate();
+				
+				if(startDateDay < 10) {
+					startDateDay = '0'+startDateDay;
+				}
+				if(endDateDay < 10) {
+					endDateDay = '0'+endDateDay;
+				}
+				var startDateMonth = startDate.getMonth()+1;
+				var startDateYear = startDate.getFullYear();
+				
+				var usersCollection = db.collection('coll_users');
+				var endDayMonth = parseInt(''+endDateMonth+''+endDateDay);
+				var startDayMonth = parseInt(''+startDateMonth+''+startDateDay);
+				var unwindvar = '$_'+endDateYear;
+				var matchKey = '_'+endDateYear+'._id';
+				
+				var query = [{"$match":{"_2016._id" : {"$gte":startDayMonth,"$lt":endDayMonth}}},{"$unwind": unwindvar},{"$group":{"_id":{"result" : "$ldt"},"users":{"$sum": 1}}}];
+				//var query = '[{"$match":{"'+matchKey+'" : {"$gte":'+startDayMonth+',"$lt":'+endDayMonth+'}}},{"$unwind": "'+unwindvar+'"},{"$group":{"_id":{"ldt" : "$ldt"},"users":{"$sum": 1}}}]';
+				
+				usersCollection.aggregate(query,function(err, items) {
+					if(err){
+						var errMsg = common.getErrorMessageFrom(err);
+						logger.error(errMsg);
+					}
+					var resultLength = items.length;
+					if(resultLength > 0){
+						// find tablet/smart phone entry
+						var smartCount;
+						var tabletCount;
+						for (var i in items) {
+							var count = items[i];
+							if(count._id.result == 'S') {
+								smartCount = count.users;
+							} else if(count._id.result == 'T'){
+								tabletCount = count.users;
+							}
+						}
+						
+						var KeyFormat = dateFormat(endDate, "yyyymmddHH");
+						var dashboardCollection = db.collection('coll_dashboard');
+					
+						//smart Hourly collection update
+						if(smartCount != null && smartCount > 0) {
+							var dashboardQuerySmart = '{"$and":[{"_id.ty":"M"}, {"_id.dt" : "S"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQuerySmartJson = JSON.parse(dashboardQuerySmart);
+							//var updateSmt = '{"$set":{"val.tuu":'+smartCount+'}}';
+							//var updateQueryJsonSmt = JSON.parse(updateSmt);
+							var updateSmt = {};
+							updateSmt['val.tuu']=smartCount;
+							dashboardCollection.update(dashboardQuerySmart, updateSmt,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+				
+						//tablet phone Hourly collection update
+						if(tabletCount != null && tabletCount > 0) {
+							var dashboardQueryTablet = '{"$and":[{"_id.ty":"M"}, {"_id.dt" : "T"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQueryTabletJson = JSON.parse(dashboardQueryTablet);
+							//var updateTab = '{"$set":{"val.tuu":'+tabletCount+'}}';
+							//var updateQueryJsonTab = JSON.parse(updateTab);
+							var updateTab = {};
+							updateTab['val.tuu']=tabletCount;
+							dashboardCollection.update(dashboardQueryTablet, updateTab ,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+						console.log('all done...!');
+					} else {
+						console.log('no record found...!');
+					}
+					db.close();
+				});
+			
+			}else if(triggerTyep == 'Yearly' || triggerTyep == 'yearly'){
+				startDate = new Date(endDate.getFullYear(),0, 1);
+				var startDateDay = startDate.getDate();
+				
+				if(startDateDay < 10) {
+					startDateDay = '0'+startDateDay;
+				}
+				if(endDateDay < 10) {
+					endDateDay = '0'+endDateDay;
+				}
+				var startDateMonth = startDate.getMonth()+1;
+				var startDateYear = startDate.getFullYear();
+				
+				var usersCollection = db.collection('coll_users');
+				var endDayMonth = parseInt(''+endDateMonth+''+endDateDay);
+				var startDayMonth = parseInt(''+startDateMonth+''+startDateDay);
+				var unwindvar = '$_'+endDateYear;
+				var matchKey = '_'+endDateYear+'._id';
+				
+				var query = [{"$match":{"_2016._id" : {"$gte":startDayMonth,"$lt":endDayMonth}}},{"$unwind": unwindvar},{"$group":{"_id":{"result" : "$ldt"},"users":{"$sum": 1}}}];
+				//var query = '[{"$match":{"'+matchKey+'" : {"$gte":'+startDayMonth+',"$lt":'+endDayMonth+'}}},{"$unwind": "'+unwindvar+'"},{"$group":{"_id":{"ldt" : "$ldt"},"users":{"$sum": 1}}}]';
+				
+				usersCollection.aggregate(query,function(err, items) {
+					if(err){
+						var errMsg = common.getErrorMessageFrom(err);
+						logger.error(errMsg);
+					}
+					var resultLength = items.length;
+					if(resultLength > 0){
+						// find tablet/smart phone entry
+						var smartCount;
+						var tabletCount;
+						for (var i in items) {
+							var count = items[i];
+							if(count._id.result == 'S') {
+								smartCount = count.users;
+							} else if(count._id.result == 'T'){
+								tabletCount = count.users;
+							}
+						}
+						
+						var KeyFormat = dateFormat(endDate, "yyyymmddHH");
+						var dashboardCollection = db.collection('coll_dashboard');
+					
+						//tablet Hourly collection update
+						if(smartCount != null && smartCount > 0) {
+							var dashboardQuerySmart = '{"$and":[{"_id.ty":"M"}, {"_id.dt" : "S"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQuerySmartJson = JSON.parse(dashboardQuerySmart);
+							//var updateSmt = '{"$set":{"val.tuu":'+smartCount+'}}';
+							//var updateQueryJsonSmt = JSON.parse(updateSmt);
+							var updateSmt = {};
+							updateSmt['val.tuu']=smartCount;
+							dashboardCollection.update(dashboardQuerySmart, updateSmt,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+				
+						//smart phone Hourly collection update
+						if(tabletCount != null && tabletCount > 0) {
+							var dashboardQueryTablet = '{"$and":[{"_id.ty":"Y"}, {"_id.dt" : "Y"},{"_id.key":{"$eq":'+parseInt(KeyFormat.toString())+'}}]}';
+							var dashboardQueryTabletJson = JSON.parse(dashboardQueryTablet);
+							//var updateTab = '{"$set":{"val.tuu":'+tabletCount+'}}';
+							//var updateQueryJsonTab = JSON.parse(updateTab);
+							var updateTab = {};
+							updateTab['val.tuu']=tabletCount;
+							dashboardCollection.update(dashboardQueryTablet, updateTab ,function( err, result ) {
+								if ( err ) {
+									var errMsg = common.getErrorMessageFrom(err);
+									logger.error(errMsg);
+								}
+							});
+						}
+						console.log('all done...!');
+					} else {
+						console.log('no record found...!');
+					}
+					db.close();
+				});
 			}
-			var startDateUnixtime = Math.round(startDate.getTime() / 1000);
-			
-			console.log('startDate : ', startDateUnixtime);
-			console.log('endDateUnixtime : ', endDateUnixtime);
-			
-			//this query will form a implicit AND (no need to decleare explicit AND)
-			var query = [{$match:{rtr:{$gte:startDateUnixtime,$lt:endDateUnixtime}}},{$group:{_id :{val:"$val.dt","val.did":"$val.did"}}},{$group:{_id:{f:'$_id.val'},"users":{$sum:1}}}];
-			
-			beginsCollection.aggregate(query,function(err, items) {
-				var resultLength = items.length;
-				
-				// find tablet/smart phone entry
-				var smartCount;
-				var tabletCount;
-				for (var i in items) {
-					var result = items[i];
-					if(result._id.f == 'S') {
-						smartCount = result.users;
-					} else if(result._id.f == 'T'){
-						tabletCount = result.users;
-					}
-				}
-			
-				if(resultLength > 0){
-					var now = new Date();
-					var dashboardKey = dateFormat(now, "yyyymmddHH");
-					console.log('key ' ,dashboardKey);
-					var dashboardCollection = db.collection('coll_dashboard');
-					
-					
-					//tablet Hourly collection update
-					if(smartCount != null && smartCount > 0) {
-						var dashboardQueryTablet = {$and:[{"_id.ty":"H"}, {"_id.dt" : "S"},{"_id.key":{$eq:dashboardKey}}]};
-						dashboardCollection.update(dashboardQueryTablet, { $set:{"val.tuu":smartCount}},function( err, result ) {
-							if ( err ) {
-								var errMsg = common.getErrorMessageFrom(err);
-								logger.error(errMsg);
-							}
-						});
-					}
-				
-					//smart phone Hourly collection update
-					if(tabletCount != null && tabletCount > 0) {
-						var dashboardQuerySmart = {$and:[{"_id.ty":"H"}, {"_id.dt" : "T"},{"_id.key":{$eq:dashboardKey}}]};
-						dashboardCollection.update(dashboardQuerySmart, { $set:{"val.tuu":tabletCount}},function( err, result ) {
-							if ( err ) {
-								var errMsg = common.getErrorMessageFrom(err);
-								logger.error(errMsg);
-							}
-						});
-					}
-				} else {
-					console.log('no record found...!');
-				}
-			db.close();
-			});
-		}
+		}	
 	});
 
 } else {
-	console.log('arguments required order is "url" " appkey" "type(Hourly/Daily)"');
+	console.log('arguments required order is "mongodb url" " appkey" "type(Hourly/Daily/Weekly/Monthly/yearly)"');
 }
