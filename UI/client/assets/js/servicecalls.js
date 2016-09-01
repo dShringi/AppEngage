40f36ae9d8a35b2ee16b8325c548eb9a57bd305c
Abhijitnair@sd-taylor.co.uk <Abhiji102491@mastek.com>
2016-09-01 16:12:33 +0530

    registration code updated

--- UI/client/assets/js/servicecalls.js
+++ UI/client/assets/js/servicecalls.js
@@ -27,10 +27,53 @@ var service = {
                 }
 
             },
-            error: function () {
-                alert("Error");
+            error: function (x, t, m) {
+                alert("Error connecting to server");
+                if (t === "timeout") {
+                    alert("timeout");
+                } else {
+                    //alert(t);
+                }
             }
 
+        });
+    },
+
+    validateUname: function (uname) {
+        console.log("val uname");
+        $.ajax({
+            type: 'GET',
+            url: "http://digiintel.com/appengage/getUserNameValidated",
+            contentType: "application/json",
+            dataType: "json",
+            timeout: 180000,  //180 sec
+            data: "username=" + uname,
+            success: function (data) {
+                sessionStorage.setItem("unameAvailability", data.msg);
+                if ($("#reg-uname").val() === "") {
+                    $("#uname-check").css("opacity", "0");
+                }
+                else if (data.msg === "Success") {
+                    $("#uname-check i.fa").removeClass("fa-close").addClass("fa-check");
+                    $("#uname-check").css("opacity", "1");
+                    $("#uname-check").css("color", "#33cc33");
+                    $("#uname-check #avail-message").html("&nbsp;&nbsp;This username is available");
+                }
+                else {
+                    $("#uname-check i.fa").removeClass("fa-check").addClass("fa-close");
+                    $("#uname-check").css("opacity", "1");
+                    $("#uname-check").css("color", "#ff3300");
+                    $("#uname-check #avail-message").html("&nbsp;&nbsp;This username is taken");
+                }
+            },
+            error: function (x, t, m) {
+                alert("Error connecting to server");
+                if (t === "timeout") {
+                    alert("timeout");
+                } else {
+                    //alert(t);
+                }
+            }
         });
     },
 	
@@ -38,18 +81,7 @@ var service = {
 		console.log("service called");
 		// Array order - First Name, Email Id, User Name, Last Name, Phone, Password, App Name, App Description, Timeout, App Category, App Icon, Timezone
 		//					0			1			2			3		4		5		6				7			8			9			10		11
-		console.log(arr[0]);
-		console.log(arr[1]);
-		console.log(arr[2]);
-		console.log(arr[3]);
-		console.log(arr[4]);
-		console.log(arr[5]);
-		console.log(arr[6]);
-		console.log(arr[7]);
-		console.log(arr[8]);
-		console.log(arr[9]);
-		console.log(arr[10]);
-		console.log(arr[11]);
+
 		var registerJSONReq = {
 			"user": {
                     "fn": arr[0],
@@ -70,6 +102,34 @@ var service = {
 		}
 		
 		console.log(registerJSONReq);
+		$.ajax({
+		    type: 'POST',
+		    url: "http://digiintel.com/appengage/registerUser",
+		    contentType: "application/json",
+		    datatype: "json",
+		    timeout: 180000,
+		    data: JSON.stringify(registerJSONReq),
+		    success: function (data) {
+		        console.log(data);
+		        $("#modalRegister").modal('hide');
+		        swal({
+		            title: 'Congratulations!',
+		            text: '<p style="font-size:14px; padding-top: 30px;">You have successfully registered with us.</p>' +
+                            '<p style="font-size:14px;">Your registration key is -</p>' +
+                            '<h3>'+data.akey+'</h3>',
+		            html: true
+		        });
+            },
+		    error: function (x, t, m) {
+		        alert("Error connecting to server");
+		        if (t === "timeout") {
+		            alert("timeout");
+		        } else {
+		            //alert(t);
+		        }
+		    }
+
+		});
 	
 	}
 	
