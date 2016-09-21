ebe5b3032d670d1b98898411b45628da57c2d764
Sanjeev Ranjan <sanjeev13540@Mastek.com>
2016-09-21 18:32:49 +0530

    campaign trigger time while pushing to db

--- UI/server/controllers/campaignController.js
+++ UI/server/controllers/campaignController.js
@@ -10,12 +10,12 @@ module.exports.createCampaign = function(req,res){
   //store it in mongodb.
   
 	var body = req.body;
-	var schdule_type = body.schdule_type;
-	if(schdule_type.toUpperCase() == 'IMMEDIATE'){
-		var currentTime = new Date();
-		currentTime.setMinutes(currentTime.getMinutes()+1);
-		body.trigger_time= parseInt(dateFormat(currentTime, "yyyymmddHHMM"));
-	}
+	var schedule_type = body.schedule_type;
+	var cycle = body.cycle;
+	var recursive = body.recursive;
+	var trigger_time = body.trigger_time;
+	var currentTime = getTriggerTime(schedule_type.toUpperCase(), cycle, recursive, trigger_time);
+	body.trigger_time= parseInt(dateFormat(currentTime, "yyyymmddHHMM"));
   
   db.collection(config.coll_campaigns).insert(req.body,function(err,res){
     if(err){
@@ -85,3 +85,121 @@ module.exports.fetchAllCampaigns = function(req,res){
   });
 
 };
+
+function getTriggerTime(schedule_type, cycle, recursive, trigger_time){
+	var returnDate = new Date();
+	if(schedule_type == 'IMMEDIATE'){
+		returnDate.setMinutes(returnDate.getMinutes()+1);
+	} else if((schedule_type == 'SCHEDULED' && recursive == true)) {
+		var cycleArray = cycle.split('_');
+		var cycleType = cycleArray[0].toUpperCase();
+		var givenHours = parseInt(trigger_time.toString().substring(8,10));
+		var givenMinutes = parseInt(trigger_time.toString().substring(10,12));
+		switch(cycleType) {
+				case 'DAILY': {
+					var dayFlag = checkGreaterTime(givenHours, givenMinutes);
+					if(dayFlag) {
+						returnDate.setHours(givenHours,givenMinutes);
+					} else {
+						returnDate.setDate(returnDate.getDate() + 1);
+						returnDate.setHours(givenHours,givenMinutes);
+					}
+				}break; 
+				case 'ALTERNATE': {
+					var dayFlag = checkGreaterTime(givenHours, givenMinutes);
+					if(dayFlag) {
+						returnDate.setHours(givenHours,givenMinutes);
+					} else {
+						returnDate.setDate(returnDate.getDate() + 2);
+						returnDate.setHours(givenHours,givenMinutes);
+					}
+				}break;
+				case 'WEEKLY': {
+					var weekDay = cycleArray[1].toUpperCase();
+					returnDate = getNextDayOfWeek(getNumberFromDay(weekDay));
+					var weekFlag = checkGreaterMonth(givenHours, givenMinutes,returnDate.getDate());
+					if(weekFlag){
+						returnDate.setHours(givenHours,givenMinutes);
+					} else {
+						returnDate.setDate(returnDate.getDate()+7);
+						returnDate.setHours(givenHours,givenMinutes);
+					}
+				}break;
+				case 'MONTHLY': {
+					var givenDate = parseInt(cycleArray[1]);
+					var monthFlag = checkGreaterMonth(givenHours,givenMinutes, givenDate);
+					if(monthFlag) {
+						returnDate.setDate(givenDate);
+						returnDate.setHours(givenHours,givenMinutes);
+					} else {
+						returnDate.setMonth(returnDate.getMonth() + 1);
+						returnDate.setDate(givenDate);
+						returnDate.setHours(givenHours,givenMinutes);
+					}
+				}break;
+		}
+	}
+	return returnDate; 
+}
+
+function getNumberFromDay(day) {
+	if(day != null) {
+		var upperCase = day.toUpperCase();
+		switch(upperCase) {
+			case 'SUNDAY': {return 0;} break;
+			case 'MONDAY': {return 1;} break;
+			case 'TUESDAY': {return 2;} break;
+			case 'WEDNESDAY': {return 3;} break;
+			case 'THURSDAY': {return 4;} break;
+			case 'FRIDAY': {return 5;} break;
+			case 'SATURDAY': {return 6;} break;
+		}
+	}
+}
+
+function getNextDayOfWeek(dayOfWeek) {
+	var date = new Date();
+    var resultDate = new Date(date.getTime());
+    resultDate.setDate(date.getDate() + (7 + dayOfWeek - date.getDay()) % 7);
+    return resultDate;
+}
+
+function checkGreaterTime(givenHours, givenMinutes) {
+	var date = new Date();
+	var currentHours = date.getHours();
+	var currentMinutes = date.getMinutes()+1;
+	if(currentHours < givenHours) {
+		return true;
+	} else if(currentHours > givenHours) {
+		return false;
+	} else if(currentHours == givenHours) {
+		if(currentMinutes < givenMinutes) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function checkGreaterMonth(givenHours, givenMinutes, givenDate) {
+	var date = new Date();
+	var currentDate = date.getDate();
+	var currentHours = date.getHours();
+	var currentMinutes = date.getMinutes()+1;
+	
+	if(currentDate < givenDate){
+		return true;
+	} else if(currentDate > givenDate) {
+		return false;
+	} else if(currentDate == givenDate) {
+		if(currentHours < givenHours) {
+			return true;
+		} else if(currentHours > givenHours) {
+			return false;
+		} else if(currentHours == givenHours) {
+			if(currentMinutes < givenMinutes) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
