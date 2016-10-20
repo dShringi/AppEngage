262f363c592fe7e3e85f82ba62fc28e9af1ee54b
Sourabh S. Jain <sourabh13506@Mastek.com>
2016-10-20 15:57:16 +0530

    Modified for image paths

--- UI/server/controllers/screenController.js
+++ UI/server/controllers/screenController.js
@@ -6,9 +6,9 @@ const logger  = require('../../config/log.js');
 const common = require('../../commons/common.js');
 */
 module.exports.fetchScreenStats = function(req,res){
-let response = '[{"name": "app1","path": "/opt/AppEngage/data/4170b44d6459bba992acaa857ac5b25d7fac6cc1/img1.jpg","alt": "app1","nuu": 13,"noc": 5,"tts": 300,"ts": 430},' +
-                 '{"name": "app2","path": "/opt/AppEngage/data/4170b44d6459bba992acaa857ac5b25d7fac6cc1/img2.jpg","alt": "app2","nuu": 23,"noc": 50,"tts": 500,"ts": 830},' +
-                 '{"name": "app2","path": "/opt/AppEngage/data/4170b44d6459bba992acaa857ac5b25d7fac6cc1/img3.jpg","alt": "app2","nuu": 33,"noc": 20,"tts": 700,"ts": 338},' +
+let response = '[{"name": "app1","path": "assets/appscreenimages/4170b44d6459bba992acaa857ac5b25d7fac6cc1/img1.jpg","alt": "app1","nuu": 13,"noc": 5,"tts": 300,"ts": 430},' +
+                 '{"name": "app2","path": "assets/appscreenimages/4170b44d6459bba992acaa857ac5b25d7fac6cc1/img2.jpg","alt": "app2","nuu": 23,"noc": 50,"tts": 500,"ts": 830},' +
+                 '{"name": "app2","path": "assets/appscreenimages/4170b44d6459bba992acaa857ac5b25d7fac6cc1/img3.jpg","alt": "app2","nuu": 33,"noc": 20,"tts": 700,"ts": 338}' +
                  ']';
 return res.json(JSON.parse(response));
 };
